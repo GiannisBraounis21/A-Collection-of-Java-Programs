54dc845e82dceee0dc6e4e9aaf885972314e8314
Gioani Braouni <62447651+GioaniBraouni@users.noreply.github.com>
2020-12-13 22:46:12 +0200

    Add files via upload

---
+++ InputChecker/src/Main/Main.java
@@ -0,0 +1,76 @@
+package Main;
+
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Random rnd = new Random(); 
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Δώσε πλήθος στοιχείων του πίνακα");
+        
+        int total=0;
+        while(true)
+        {
+            try
+            {
+                total = input.nextInt(); 
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Λαθος εισοδος , δωσε ακεραιο");
+                System.err.println("Not a integer number");
+                input.nextLine();
+            }
+            if(total!=0)
+                break;
+        }
+        double array [] = null;
+        try
+        {
+            array = new double [total];
+        }
+        catch(NegativeArraySizeException e)
+        {
+            System.err.println("Error: cannot create array with negative size");
+            System.exit(1);
+        }
+        
+        for(int i=0; i<total; i++)
+        {
+            array[i] = rnd.nextInt(100);
+        }
+        
+        int index;
+        int counter=0;
+        while(counter<5)
+        {
+            index=0;
+            try
+            {
+                System.out.println("Ποιο στοιχειο του πινακα θες να εκτυπωσεις?");
+                index = input.nextInt();
+                System.out.println("Array element" + index + ": " + array[index]);
+
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Λαθος εισοδος , δωσε ακεραιο");
+                System.err.println("Not a integer number");
+            }
+            catch(ArrayIndexOutOfBoundsException e)
+            {
+                System.err.println("Array index out of bounds");
+            }
+            input.nextLine();
+            if(index>=0 && index<array.length)
+            {
+                counter++;
+            }
+        }
+        System.out.println("Εxit");
+    }
+    
+}
